c9f4352efaaaaaf1b6c64a82a8cf56a6cb737c26
Lucky_cat_133 <xingmiao201081@gmail.com>
2026-02-22 22:01:41 +0800

    Initial Commit

---
+++ Chat_Buddy_iOS/Chat_Buddy_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_Buddy_iOSApp.swift
+//  Chat_Buddy_iOS
+//
+//  Created by Jack on 2026/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Chat_Buddy_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
